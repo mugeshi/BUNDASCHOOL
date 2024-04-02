fa90952a8f439e007f0da06c6d87bbf3509133b2
mugeshi <nicolemugeshi@gmail.com>
2024-04-02 22:28:34 +0300

    updated MenuApi

---
+++ RESTRAUNT/src/MenuApi.js
@@ -0,0 +1,131 @@
+const Menu = [
+  {
+    id: 1,
+    image: "images/allupakoida.jpg",
+    name: "allu pakoida",
+    category: "snacks",
+    price: "20₹",
+    description:
+      "Pakoras are super crisp on the outside because of their deep-fried battered layer, while the insides are tender-soft. The color of a well made pakora is pretty gold, and the taste is savory, spicy, and greasy.  ",
+  },
+
+  {
+    id: 2,
+    image: "images/chola.jpg",
+    name: "chola",
+    category: "breakfast",
+    price: "70₹",
+    description:
+      " A popular North Indian dish made from chickpeas cooked in a flavorful blend of spices and herbs, offering a hearty and protein-rich option."
+  },
+  {
+    id: 3,
+    image: "images/corn.jpg",
+    name: "corn",
+    category: "snacks",
+    price: "20₹",
+    description:
+      " A versatile ingredient that can be served as boiled, roasted, or in dishes like corn salad, corn on the cob, or corn soup, known for its sweet and crunchy kernels."
+  },
+  {
+    id: 4,
+    image: "images/maggi.jpg",
+    name: "maggie",
+    category: "snacks",
+    price: "24₹",
+    description:
+      "Instant noodles loved by many, usually served with a quick and delicious mix of spices and sauces, making it a convenient snack."
+  },
+  {
+    id: 5,
+    image: "images/momo.jpg",
+    name: "momo",
+    category: "snacks",
+    price: "80₹",
+    description:
+      "A beloved dumpling dish hailing from Tibet, often filled with minced meat or vegetables, served with a dipping sauce like spicy chutney."
+  },
+  {
+    id: 6,
+    image: "images/nonvegthali.jpg",
+    name: "Non-Veg Thali",
+    category: "dinner",
+    price: "180₹",
+    description:
+      " A well-rounded Indian meal featuring a variety of non-vegetarian dishes such as curry, meat, rice, bread, and accompaniments."
+  },
+  {
+    id: 7,
+    image: "images/paubhaji.jpg",
+    name: "pav bhaji",
+    category: "dinner",
+    price: "60₹",
+    description:
+      "A popular Mumbai street food, consisting of spiced mashed vegetables (bhaji) served with soft bread rolls (pav), a delightful combination."
+  },
+  {
+    id: 8,
+    image: "images/pizza.jpg",
+    name: "Pizza",
+    category: "snacks",
+    price: "60₹",
+    description:
+      "An Italian classic now enjoyed worldwide, featuring a thin or thick crust topped with tomato sauce, cheese, and various toppings, baked to perfection."
+  },
+  {
+    id: 9,
+    image: "images/puri.jpg",
+    name: "puri",
+    category: "breakfast",
+    price: "30₹",
+    description:
+      "A deep-fried bread from India, known for its airy texture, often served with sides like sabzi or as a dessert with sugar and ghee."
+  },
+  {
+    id: 10,
+    image: "images/rajmarice.jpg",
+    name: "rajma rice",
+    category: "dinner",
+    price: "200₹",
+    description:
+      " A North Indian favorite, this dish combines kidney beans in a rich tomato-based gravy, often served with steamed rice for a comforting meal."
+  },
+  {
+    id: 11,
+    image: "images/rotiwithmeat.jpg",
+    name: "Roti With Meat",
+    category: "dinner",
+    price: "220₹",
+    description:
+      " A traditional Indian meal where flatbreads (roti) are paired with a meat dish, providing a balance of flavors and textures."
+  },
+  {
+    id: 12,
+    image: "images/samosa.jpg",
+    name: "samaso",
+    category: "snacks",
+    price: "15₹",
+    description:
+      "A popular snack worldwide, this deep-fried pastry is typically stuffed with spiced potatoes, peas, and sometimes minced meat."
+  },
+  {
+    id: 13,
+    image: "images/sweet.jpg",
+    name: "sweet",
+    category: "desert",
+    price: "150₹",
+    description:
+      "A diverse category of desserts found in various cultures, such as Indian sweets like gulab jamun, rasgulla, or Western options like chocolate cake and ice cream."
+  },
+  {
+    id: 14,
+    image: "images/vegthali.jpg",
+    name: "vegthali",
+    category: "lunch",
+    price: "150₹",
+    description:
+      " A complete vegetarian meal that includes a variety of dishes like vegetables, lentils, rice, bread, and condiments, offering a wholesome dining experience."
+  },
+];
+
+export default Menu;
